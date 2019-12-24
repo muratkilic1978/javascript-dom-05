7c8e75655c4b3f1971fd0591999842ead6b423f8
muratkilic1978 <muratkilic1978@gmail.com>
2019-12-24 11:32:35 +0100

    manipulation.js was updated

--- js/manipulation.js
+++ js/manipulation.js
@@ -4,7 +4,7 @@ let taxElem = document.querySelector("#tax");
 let totalElem = document.querySelector("#total");
 let totalPrice = 0;
 
-
+//textContent Gets/Sets text only
 subtotalElem.textContent = 17;
 console.log(subtotalElem);
 shippingElem.textContent = 4.75;
@@ -13,7 +13,28 @@ taxElem.textContent = 1.74;
 console.log(taxElem);
 totalPrice = (parseFloat(subtotalElem.innerText)+parseFloat(shippingElem.innerText)+parseFloat(taxElem.innerText));
 totalElem.textContent = totalPrice;
+console.log(totalElem);
 
+// textContent Gets/Sets & markup
+//subtotalElem.innerHTML = 17;
+//console.log(subtotalElem);
+//shippingElem.innerHTML = 4.75;
+//console.log(shippingElem);
+//taxElem.innerHTML = 1.74;
+//console.log(taxElem);
+//totalPrice = (parseFloat(subtotalElem.innerHTML))+(parseFloat(shippingElem.innerHTML))+(parseFloat(taxElem.innerHTML));
+//totalElem.innerHTML = totalPrice;
+//console.log(totalPrice);
 
+// textContent Gets/Sets text only
+//subtotalElem.innerText = 17;
+//console.log(subtotalElem);
+//shippingElem.innerText = 4.75;
+//console.log(shippingElem);
+//taxElem.innerText = 1.74;
+//console.log(taxElem);
+//totalPrice = (parseFloat(subtotalElem.innerText))+(parseFloat(shippingElem.innerText))+(parseFloat(taxElem.innerText));
+//totalElem.innerText = totalPrice;
+//console.log(totalPrice);
 
 
